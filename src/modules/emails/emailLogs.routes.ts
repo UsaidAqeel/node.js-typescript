81e7c0d331685a6eb536bdb6f4427bfe4d36e19c
Usaid Aqeel <usaidaqeel309@outlook.com>
2025-01-31 04:02:17 +0500

    feat: refactor authentication middleware and integrate with email logs route

--- src/modules/emails/emailLogs.routes.ts
+++ src/modules/emails/emailLogs.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { getAllEmailLogsController } from "./emailLogs.controller";
+import { authGuard } from "src/common/middlewares/auth";
 
 export const emailLogRouter = Router();
 
-emailLogRouter.route("/email-logs").get(getAllEmailLogsController);
+emailLogRouter.route("/email-logs").get(authGuard, getAllEmailLogsController);
